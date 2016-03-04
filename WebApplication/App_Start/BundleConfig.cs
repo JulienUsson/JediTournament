f1c170d6c8f560789c8a9f6abea321e0ecfd9915
JulienUsson <usson.julien@gmail.com>
2016-03-04 18:57:49 +0100

    Modif du design

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace WebApplication
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/particles").Include(
+                      "~/Scripts/particles.min.js",
+                      "~/Scripts/particles-app.js"));
         }
     }
 }
